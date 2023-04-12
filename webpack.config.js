fc31c77c0741571d409591e3b0b28cf94bc1f9cc
zaizaizhao <2534243725@qq.com>
2023-04-13 00:09:59 +0800

    mock server established

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,38 @@
 const path = require('path');
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    context:process.cwd(),
-    entry:'./src/index.js',
-    mode:"development",
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:"monitor.js"
-    },
-    devServer:{
-        static:path.resolve(__dirname,'dist'),
-        historyApiFallback: true,
-    },
-    plugins:[
-        new HTMLWebpackPlugin({
-            template:"./src/index.html",
-            inject:"head"
-        })
-    ]
+  context: process.cwd(),
+  entry: './src/index.js',
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "monitor.js"
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    // historyApiFallback: true,
+    // 实际上是node的express服务
+    setupMiddlewares: (middlewares, devServer) => {
+      devServer.app.get('/success', (_, response) => {
+        console.log(234);
+        response.json('setup-middlewares option GET');
+      });
+      devServer.app.post('/error', (_, response) => {
+        response.json('setup-middlewares option GET');
+      });
+      middlewares.unshift({
+        name: 'first-in-array',
+        middleware: (req, res) => {
+          res.send('Foo!');
+        },
+      });
+      return middlewares;
+    }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      inject: "head"
+    })
+  ]
 }
