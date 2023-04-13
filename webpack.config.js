be2602949845e12f6dc47b60c0585d28d28eda98
zaizaizhao <2534243725@qq.com>
2023-04-13 23:20:09 +0800

    deverver

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  context: process.cwd(),
+  // context: process.cwd(),
   entry: './src/index.js',
   mode: "development",
   output: {
@@ -9,25 +9,35 @@ module.exports = {
     filename: "monitor.js"
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
+    // static: path.resolve(__dirname, 'dist'),
     // historyApiFallback: true,
     // 实际上是node的express服务
     setupMiddlewares: (middlewares, devServer) => {
-      devServer.app.get('/success', (_, response) => {
-        console.log(234);
-        response.json('setup-middlewares option GET');
-      });
-      devServer.app.post('/error', (_, response) => {
-        response.json('setup-middlewares option GET');
-      });
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
+      // 如果你想在所有其他中间件之前运行一个中间件或者当你从 `onBeforeSetupMiddleware` 配置项迁移时，
+      // 可以使用 `unshift` 方法
       middlewares.unshift({
-        name: 'first-in-array',
+        name: "",
+        // `path` 是可选的
+        path: '/success',
         middleware: (req, res) => {
-          res.send('Foo!');
+          res.json({ message: "success request" });
+        },
+      });
+      // 如果你想在所有其他中间件之后运行一个中间件或者当你从 `onAfterSetupMiddleware` 配置项迁移时，
+      // 可以使用 `push` 方法
+      middlewares.push({
+        // `path` 是可选的
+        path: '/error',
+        middleware: (req, res) => {
+          res.json({ error: "error request" });
         },
       });
       return middlewares;
-    }
+    },
   },
   plugins: [
     new HTMLWebpackPlugin({
